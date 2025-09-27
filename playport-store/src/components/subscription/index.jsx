62fa2d19c6f5a4852cdbcc7ae6d355f22a2cb955
Mur4dd <muradahmadli1208@gmail.com>
2025-09-27 11:15:59 +0400

    subscriptionUpdate

--- playport-store/src/components/subscription/index.jsx
+++ playport-store/src/components/subscription/index.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function index() {
+
+    const [email, setEmail] = useState("")
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)  
+    }
+
   return (
     <div className='bg-blue-800 uppercase text-white font-montserrat flex flex-col gap-7 pt-8 pb-8 pl-2.5 pr-2.5'>
         <div className='text-center flex flex-col gap-2.5 md:text-[14px] lg:text-[18px] xl:text-[20px]'>
@@ -9,7 +16,12 @@ function index() {
         </div>
         <div className='flex justify-center'>
             <div className='border-white border-2 rounded-[5px] flex justify-between'>
-                <input type="email" name="email" id="email" className='outline-0 p-1 md:w-[400px]'/>
+                <input type="email"
+                name="email" 
+                id="email" 
+                className='outline-0 p-1 md:w-[400px]'
+                onChange={handleEmail}
+                />
                 <input type="submit" value="Göndər" className='outline-0 border-0 bg-white text-blue-800 font-bold h-full p-2 hover:bg-blue-800 hover:text-white transition duration-300 cursor-pointer'/>
             </div>
         </div>
